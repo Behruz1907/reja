3bb641bc8e06656d6ef1e4d364dc0127857bf346
Behruz <behruzanorboev1@gmail.com>
2026-04-21 15:18:17 +0900

    feat: Frontend section added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const { text } = require("stream/consumers");
 
 // 1: Kirish code
 
@@ -18,13 +19,24 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
-app.get("/hello", function (req, res) {
-  res.end(`<h1>HELLO WORLD</h1>`);
+app.post("/create-item", (req, res) => {
+  // REQ==  3SECTIONS 1-START LINE, URL QISMI! 2-HTTP REQUEST HEADER QISMI! 3-BODY QISMI
+  // POST OZI BN MALUM BIR MALUMOTNI OLIB KELADI VA DATA BASE GA OSHA MALUMOTNI YOZADI!!!
+  console.log(req);
+  res.json({ test: "success" });
 });
 
-app.get("/gift", function (req, res) {
-  res.end(`<h1> You are on gift page</h1>`);
+app.get("/", function (req, res) {
+  // GET ---> DATABASE DAN MALUMOTNI OLISH UCHUN GET ISHLATILADI!!!!
+  res.render("harid");
 });
+// app.get("/hello", function (req, res) {
+//  res.end(`<h1>HELLO WORLD</h1>`);
+// });
+
+// app.get("/gift", function (req, res) {
+//   res.end(`<h1> You are on gift page</h1>`);
+// });
 
 const server = http.createServer(app);
 let PORT = 3000;

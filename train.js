2b5789005071bf02407dacd19b31f6c76cb38004
Behruz <behruzanorboev1@gmail.com>
2026-04-25 02:35:35 +0900

    feat: completed task a

--- train.js
+++ train.js
@@ -1,47 +1,84 @@
 //  npm run train
 
 
+/* IZOHI task-A
+
+
+Harf sifatida kiritilgan birinchi parametr, 
+kiritilgan ikkinchi parametr tarkibida nechta ekanligini qaytaruvchi
+Funktsiya tuzing
+
+Masalan: countLetter("e", "engineer")
+'engineer' so'zi tarkibida 'e' harfi 3 marotaba takrorlanganligi uchun
+3 sonini qaytaradi
+*/
 
 
 
-
-console.log("Jack Ma maslahatlari");
-const list = [
-    "yaxshi talaba boling", // 0-20
-    "togri boshliq tanlang va koproq hato qiling", // 20-30
-    "uzingizga ishlashni boshlang", // 30-40
-    "siz kuchli bolgan narsalarni qiling", // 40-50
-    "yoshlarga investitsiya qiling", // 50-60
-    "endi dam oling va yoshlarga tajriba va bilimlaringizni ulashing", // 60
-];
+/* JAVOB:*/
 
 
-// CALLBACK FUNCTIONS
 
-function maslahatBering(a, callback) {
-    if (typeof a !== 'number') callback("insert a number", null);
-    else if (a <= 20) callback(null, list[0]);
-    else if (a > 20 && a <= 30) callback(null, list[1]);
-    else if (a > 30 && a <= 40) callback(null, list[2]);
-    else if (a > 40 && a <= 50) callback(null, list[3]);
-    else if (a > 50 && a <= 60) callback(null, list[4]);
-    else {
-       
+function countLetter(coder, engineer) {
+    let  count = 0;
 
-        setTimeout(function () {
-            callback(null, list[5]);
-        }, 1000);
+    for (let i = 0; i < engineer.length; i++) {
+           if (coder === engineer[i]) {
+        count++;
     }
-
+    
+      
+    }
+    return count;
+    
+ 
 }
 
-console.log("passed here 0");   
-maslahatBering(65, (err, data) => {
-    if (err) console.log('ERROR:', err);
-    console.log('javob:', data);
-});
+console.log(countLetter("n", "engineer"));
 
-console.log("passed here 1");
+
+
+
+
+
+
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//     "yaxshi talaba boling", // 0-20
+//     "togri boshliq tanlang va koproq hato qiling", // 20-30
+//     "uzingizga ishlashni boshlang", // 30-40
+//     "siz kuchli bolgan narsalarni qiling", // 40-50
+//     "yoshlarga investitsiya qiling", // 50-60
+//     "endi dam oling va yoshlarga tajriba va bilimlaringizni ulashing", // 60
+// ];
+
+
+// // CALLBACK FUNCTIONS
+
+// function maslahatBering(a, callback) {
+//     if (typeof a !== 'number') callback("insert a number", null);
+//     else if (a <= 20) callback(null, list[0]);
+//     else if (a > 20 && a <= 30) callback(null, list[1]);
+//     else if (a > 30 && a <= 40) callback(null, list[2]);
+//     else if (a > 40 && a <= 50) callback(null, list[3]);
+//     else if (a > 50 && a <= 60) callback(null, list[4]);
+//     else {
+       
+
+//         setTimeout(function () {
+//             callback(null, list[5]);
+//         }, 1000);
+//     }
+
+// }
+
+// console.log("passed here 0");   
+// maslahatBering(65, (err, data) => {
+//     if (err) console.log('ERROR:', err);
+//     console.log('javob:', data);
+// });
+
+// console.log("passed here 1");
 
 // event loop---
 

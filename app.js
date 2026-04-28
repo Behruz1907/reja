10ea6439c221242c9b4531210e87b178d25a4e04
Behruz <behruzanorboev1@gmail.com>
2026-04-28 13:26:49 +0900

    feat: mongoDB ulanish logic

---
+++ app.js
@@ -0,0 +1,75 @@
+//  npm run dev
+
+//MOXIYAT
+
+// FRONTENDNI 2 XIL USULDA QURAMIZ
+// 1-TRADITIONAL BSSR BIZNING       HOLATDA  EJS ORQALI
+// 2- USUL BU ZAMONAVIY SINGLE PAGE APPLICATION USULI (SOON)
+
+// EXPRESS FRAMEWORK Express — bu Node.js uchun yaratilgan eng mashhur web frameworklardan biri bo‘lib, server-side (backend) ilovalarni tez va oson qurish uchun ishlatiladi.
+
+// EJS (Embedded JavaScript) — bu Node.js muhitida ishlatiladigan template engine bo‘lib, HTML ichida JavaScript kod yozishga imkon beradi.
+
+console.log("Web Serverni boshlash"); // server.js ishlayotganini visual korsatib beradi(easy)
+const express = require("express");
+const app = express(); // APP  endi meni server obyektim barcha yollar va buyruqlar aynan shu APP  orqali boshqariladi!!!!
+ // http bizning core modulimiz
+const { text } = require("stream/consumers");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+
+// MongoDB connect
+
+// 1: Kirish code
+
+app.use(express.static("public")); //stashqaridan kirib  kelayotgan foydalanuvhcuilar malumotlar ochiq , cs, image qoyamiz
+app.use(express.json()); // BU KIRIB KELGAN json formatdagi datani object ko'rinishga ogirib beradi (client va web serverlar ortasidagi data json format ko'rinishida)
+app.use(express.urlencoded({ extended: true })); // BU KOD BN HTML (TRADITIONAL)FORMDAN YOZILGAN KODLARNI EXPRESS QABUL QIVOLADI
+
+// 2 Session code
+
+// 3 Views code   // BSSR: BACKEND SERVER SITE RENDRING backendda html yasab uni clientga   o'sha viewni yasash uchun ejs dan foydalanamiz
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 Routing code
+app.post("/create-item", (req, res) => {
+  // http==  3SECTIONS 1-START LINE, URL QISMI! 2-HTTP REQUEST HEADER QISMI! 3-BODY QISMI
+  // POST OZI BN MALUM BIR MALUMOTNI OLIB KELADI VA DATA BASE GA OSHA MALUMOTNI YOZADI!!!
+  console.log(req);
+  res.json({ test: "success" });
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
+
+app.get("/", function (req, res) {
+  // GET ---> DATABASE DAN MALUMOTNI OLISH UCHUN GET ISHLATILADI!!!!
+  res.render("reja");
+});
+// app.get("/hello", function (req, res) {
+//  res.end(`<h1>HELLO WORLD</h1>`);
+// });
+
+// app.get("/gift", function (req, res) {
+//   res.end(`<h1> You are on gift page</h1>`);
+// });
+
+// const server = http.createServer(app);
+// let PORT = 3000; // PORT — bu server ishlaydigan raqamli eshik (port) degani.
+// server.listen(PORT, function () {
+//   console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+
+//   );
+// });  
+       
+module.exports = app;
--- server.js
+++ server.js
@@ -1,72 +1,27 @@
-//  npm run dev
 
-//MOXIYAT
 
-// FRONTENDNI 2 XIL USULDA QURAMIZ
-// 1-TRADITIONAL BSSR BIZNING       HOLATDA  EJS ORQALI
-// 2- USUL BU ZAMONAVIY SINGLE PAGE APPLICATION USULI (SOON)
+const http = require("http"); const mongodb = require("mongodb"); let db; const connectionString = "mongodb+srv://behruz_anorboev:1oUxz5sYLre4EPod@cluster0.fulolc8.mongodb.net/reja?retryWrites=true&w=majority";
 
-// EXPRESS FRAMEWORK Express — bu Node.js uchun yaratilgan eng mashhur web frameworklardan biri bo‘lib, server-side (backend) ilovalarni tez va oson qurish uchun ishlatiladi.
 
-// EJS (Embedded JavaScript) — bu Node.js muhitida ishlatiladigan template engine bo‘lib, HTML ichida JavaScript kod yozishga imkon beradi.
 
-console.log("Web Serverni boshlash"); // server.js ishlayotganini visual korsatib beradi(easy)
-const express = require("express");
-const app = express(); // APP  endi meni server obyektim barcha yollar va buyruqlar aynan shu APP  orqali boshqariladi!!!!
-const http = require("http"); // http bizning core modulimiz
-const { text } = require("stream/consumers");
-const fs = require("fs");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data);
-  }
+
+
+
+mongodb.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, client) => {
+    if (err) console.log("ERROR on connection  MongoDb");
+    else {
+        console.log("MongoDB connection succeed");
+        // console.log(client)
+        module.exports = client;
+        const app = require('./app');
+        const server = http.createServer(app);
+        let PORT = 3000;
+        server.listen(PORT, function () {
+            console.log(`The server is running successfully on port ===> ${PORT} , http://localhost:${PORT}`);
+        })
+    }
 });
-
-// 1: Kirish code
-
-app.use(express.static("public")); //stashqaridan kirib  kelayotgan foydalanuvhcuilar malumotlar ochiq , cs, image qoyamiz
-app.use(express.json()); // BU KIRIB KELGAN json formatdagi datani object ko'rinishga ogirib beradi (client va web serverlar ortasidagi data json format ko'rinishida)
-app.use(express.urlencoded({ extended: true })); // BU KOD BN HTML (TRADITIONAL)FORMDAN YOZILGAN KODLARNI EXPRESS QABUL QIVOLADI
-
-// 2 Session code
-
-// 3 Views code   // BSSR: BACKEND SERVER SITE RENDRING backendda html yasab uni clientga   o'sha viewni yasash uchun ejs dan foydalanamiz
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4 Routing code
-app.post("/create-item", (req, res) => {
-  // http==  3SECTIONS 1-START LINE, URL QISMI! 2-HTTP REQUEST HEADER QISMI! 3-BODY QISMI
-  // POST OZI BN MALUM BIR MALUMOTNI OLIB KELADI VA DATA BASE GA OSHA MALUMOTNI YOZADI!!!
-  console.log(req);
-  res.json({ test: "success" });
-});
-
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
-
-app.get("/", function (req, res) {
-  // GET ---> DATABASE DAN MALUMOTNI O  LISH UCHUN GET ISHLATILADI!!!!
-  res.render("reja");
-});
-// app.get("/hello", function (req, res) {
-//  res.end(`<h1>HELLO WORLD</h1>`);
-// });
-
-// app.get("/gift", function (req, res) {
-//   res.end(`<h1> You are on gift page</h1>`);
-// });
-
-const server = http.createServer(app);
-let PORT = 3000; // PORT — bu server ishlaydigan raqamli eshik (port) degani.
-server.listen(PORT, function () {
-  console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
-
-  );
-});  
-                                          
